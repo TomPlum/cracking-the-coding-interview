8c94f34b9c824a5f8b86e6615101498d8958428a
TomPlum <Thomas.Plumpton@hotmail.co.uk>
2019-09-18 15:53:00 +0100

    Question 1.3 | Added Solution & Fixed Unit Tests

--- src/main/java/com/github/tomplum/chapter1/question1/SolutionStringUtility.java
+++ src/main/java/com/github/tomplum/chapter1/question1/SolutionStringUtility.java
@@ -7,7 +7,7 @@ package com.github.tomplum.chapter1.question1;
  */
 public class SolutionStringUtility implements StringUtility {
     @Override
-    public boolean containsUniqueCharacters(final String input) {
+    public boolean containsUniqueCharacters(String input) {
         if (input == null) return true;
 
         int checker = 0;
@@ -39,6 +39,34 @@ public class SolutionStringUtility implements StringUtility {
 
     @Override
     public String encodeSpaces(String input, int trueLength) {
-        return null;
+        char[] str = input.toCharArray();
+        int numberOfSpaces = countOccurrencesOfChar(str, 0, trueLength, ' ');
+        int newIndex = trueLength - 1 + numberOfSpaces * 2;
+
+        /* If there are excess spaces, add a null character. This indicates that the spaces after that point have
+        not been replace with %20 */
+        if (newIndex + 1 < str.length) str[newIndex + 1] = '\0';
+        for (int oldIndex = trueLength - 1; oldIndex >= 0; oldIndex -= 1) {
+            if (str[oldIndex] == ' ') {
+                str[newIndex] = '0';
+                str[newIndex - 1] = '2';
+                str[newIndex - 2] = '%';
+                newIndex -= 3;
+            } else {
+                str[newIndex] = str[oldIndex];
+                newIndex -= 1;
+            }
+        }
+
+        return new String(str);
+    }
+
+
+    private int countOccurrencesOfChar(char[] str, int start, int end, int target) {
+        int count = 0;
+        for (int i = start; i < end; i++) {
+            if (str[i] == target) count++;
+        }
+        return count;
     }
 }
--- src/test/java/com/github/tomplum/chapter1/question1/SolutionStringUtilityTest.java
+++ src/test/java/com/github/tomplum/chapter1/question1/SolutionStringUtilityTest.java
@@ -1,6 +1,8 @@
 package com.github.tomplum.chapter1.question1;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -14,38 +16,95 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class SolutionStringUtilityTest {
     private final StringUtility utility = new SolutionStringUtility();
 
-    //Question 1.1
+    @Tag("Question 1.1")
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"abcdef", "kjsopw", "lkqwer", "zxcvbnm"})
     @DisplayName("Input string contains only characters that are unique. Should return true.")
-    void containsOnlyUnique(final String input) {
-        final boolean result = utility.containsUniqueCharacters(input);
+    void containsOnlyUnique(String input) {
+        boolean result = utility.containsUniqueCharacters(input);
         assertThat(result).isTrue();
     }
 
+    @Tag("Question 1.1")
     @ParameterizedTest
     @ValueSource(strings = {"aabcdfre", "aabcd66fre", "aabcd66fr--;;e"})
     @DisplayName("Input string contains duplicate characters. Should return false.")
-    void containsDuplicate(final String input) {
-        final boolean result = utility.containsUniqueCharacters(input);
+    void containsDuplicate(String input) {
+        boolean result = utility.containsUniqueCharacters(input);
         assertThat(result).isFalse();
     }
 
-    //Question 1.2
+    @Tag("Question 1.2")
     @ParameterizedTest
     @CsvSource({"abcd, dcba", "1234, 4312", "-/;@, @-/;"})
     @DisplayName("Given that the two strings are permutations of each other, should return true.")
-    void stringsArePermutationsOfEachOther(final String input, final String comparator) {
-        final boolean result = utility.stringsArePermutation(input, comparator);
+    void stringsArePermutationsOfEachOther(String input, String comparator) {
+        boolean result = utility.stringsArePermutation(input, comparator);
         assertThat(result).isTrue();
     }
 
+    @Tag("Question 1.2")
     @ParameterizedTest
     @CsvSource({"abcd, dcgba", "1234, 412", "-/;@, -/;", "abcdefghi, abcde"})
     @DisplayName("Given that the two strings are not permutations of each other, should return false.")
-    void stringsAreNotPermutationsOfEachOther(final String input, final String comparator) {
-        final boolean result = utility.stringsArePermutation(input, comparator);
+    void stringsAreNotPermutationsOfEachOther(String input, String comparator) {
+        boolean result = utility.stringsArePermutation(input, comparator);
         assertThat(result).isFalse();
+    }
+
+    @Tag("Question 1.3")
+    @ParameterizedTest
+    @CsvSource({" a , 1", "  a  , 1"})
+    void stringContainsSpaces(String input, int trueLength) {
+        String result = utility.encodeSpaces(input, trueLength);
+    }
+
+    @Test
+    @Tag("Question 1.3")
+    @DisplayName("When the input string contains a single space separating two words, it should encode that single space")
+    void singleSpaceBetweenWords() {
+        String input = "Thomas Plumpton  ";
+        int trueLength = 15;
+
+        String result = utility.encodeSpaces(input, trueLength);
+
+        assertThat(result).isEqualTo("Thomas%20Plumpton");
+    }
+
+    @Test
+    @Tag("Question 1.3")
+    @DisplayName("When the input string is just a single space, it should just return %20.")
+    void stringIsOnlyOneSpace() {
+        String input = "   ";
+        int trueLength = 1;
+
+        String result = utility.encodeSpaces(input, trueLength);
+
+        assertThat(result).isEqualTo("%20");
+    }
+
+    @Test
+    @Tag("Question 1.3")
+    @DisplayName("When the input string contains multiple words separated by spaces, it should encode all spaces.")
+    void multipleWordsSeparatedBySpaces() {
+        String input = "a b c    ";
+        int trueLength = 5;
+
+        String result = utility.encodeSpaces(input, trueLength);
+
+        assertThat(result).isEqualTo("a%20b%20c");
+    }
+
+    @Test
+    @Tag("Question 1.3")
+    @DisplayName("When the string contains two consecutive spaces, it should encode both of them.")
+    void stringContainsTwoConsecutiveSpaces() {
+        String input = "a  b    ";
+        int trueLength = 4;
+
+        String result = utility.encodeSpaces(input, trueLength);
+
+        assertThat(result).isEqualTo("a%20%20b");
     }
 }
